35cb6f8c80869ce4637e77cd782f5186e482a25f
CtrlX <CtrlX@macMini>
2014-08-08 07:22:20 -0400

    added front site

---
+++ Snoozi-AppEngine/war/wnw/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+		<script src="js/modal.js" type="text/javascript"></script>
+		<link rel="stylesheet" type="text/css" href="css/core.css">
+		<link rel="stylesheet" type="text/css" href="css/secondary.css">
+		<link href='http://fonts.googleapis.com/css?family=Ek+Mukta:200,300' rel='stylesheet' type='text/css'>
+		
+		
+		<title>
+			Wake & Wank
+		</title>
+	</head>
+	<body>
+		
+		<div id="loader">
+			
+			<div class="loader-image">
+				<a href="#main-3"><img src="css/static/loader.gif"/></a>
+			</div>
+		</div>
+		<div class="main-top" id="MyElement">
+			<div class="container">
+				<div class="row hidden-xs">
+					<div class="hidden-xs col-sm-3 col-md-3 col-lg-2 text-center shadow-text" style="margin-top:50px;">
+						<div class="logo">
+							<img src="css/static/logo-adapt.png" class="img-responsive">
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3 text-left" style="margin-top:212px;">
+							<h3 class="white-text shadow-text main-text"></h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="visible-xs col-xs-12" style="margin-top:25%;margin-bottom:8%;">
+						<div class="logo logo2">
+							<img src="css/static/words.png" class="img-responsive">
+						</div>
+					</div>
+				</div>
+				<div class="row">
+				</div>
+				<div class="row">
+					<div class="col-sm-12 col-md-8 col-lg-6 text-left"> 
+						<!--
+<div class="android-btn white-text shadow-text-strong" >
+							<p style="line-height:41pt;">Coming Soon on</p>
+							<h3 style="color:#D5FF99;margin-top:-25px;">Android</h3>
+						</div>
+-->
+					<div class="android-btn-real hidden-xs">
+					<a href="https://play.google.com/store/apps/details?id=com.wake.wank">
+							<img alt="Android app on Google Play"
+       src="css/static/gp.png" width="175"/>
+	   				</a>
+					</div>
+					
+					</div>
+				</div>
+				<div class="row" style="margin-top:2px;">
+					<div class="hidden-xs col-sm-8 white-text small">
+						<p>Wake&Wank © 2014</p>
+						<a href="mailto:"><p style="margin-top:-25px;text-decoration:none;">Contact</p></a>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 text-center" >
+						<div class="arrow hidden-xs">
+						</div>
+					</div>
+				</div>
+                <div class="row">
+                    <div class="col-xs-2 col-xs-offset-3">
+                         <a>
+							<div class="visible-xs inline-bl">
+				
+						<button type="button" class="btn btn-info btn-sm letters" style="font-weight:bold;margin-left:auto;margin-right:auto;width:100%;margin-bottom:5%;" onclick="window.location='https://www.facebook.com/pages/WakeWank/311905402310266';">
+							<img src="css/static/facebook.png" height="22" style="margin-top:-20px;margin-bottom:-20px;margin-left:-3px;">
+							</button>
+				
+							</div>
+						</a>
+                    </div>
+                    <div class="col-xs-2">
+                        <a>
+							<div class="visible-xs inline-bl">
+
+						<button type="button" class="btn btn-info btn-sm letters" style="font-weight:bold;margin-left:auto;margin-right:auto;width:100%;" onclick="window.location='https://twitter.com/WakeWankster'">
+							<img src="css/static/twitter.png" height="20" style="margin-top:-20px;margin-bottom:-20px;margin-left:-6px">
+							</button>
+				
+							</div>
+						</a>
+                    </div>
+                    <div class="col-xs-2">
+						<a data-toggle="modal" data-target="#myModal">
+							<div class="visible-xs inline-bl">
+								<button type="button" class="btn btn-default btn-sm letters" style="font-weight:bold;width:100%;" onclick="window.location='https://www.youtube.com/channel/UCxQgD16--WEAfHBryFCpM_w'">
+									<img src="css/static/youtube.png" height="20" style="margin-top:-23px;margin-bottom:-23px;margin-left:-6px"> </button>
+							</div>
+						</a>
+                    </div>
+                </div>
+                <div class="row">
+	                	<div class="android-btn-real-min visible-xs col-centered">
+							<a href="https://play.google.com/store/apps/details?id=com.wake.wank">
+								<img alt="Android app on Google Play"
+	       src="css/static/gp.png" width="175" class="img-responsive" />
+		   					</a>
+						</div>
+                </div>
+                <div class="row" style="margin-top:2px;">
+					<div class="visible-xs col-sm-8 white-text small">
+						<div class="android-btn-real-min col-centered">
+						<p class="hidden-xs">Wake&Wank © 2014</p>
+						<a href="mailto:" ><p style="margin-top:-25px;text-decoration:none;text-align:center;font-weight:bold;">Contact</p></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<a>
+			<div class="far-right4 hidden-xs">
+				
+				<button type="button" class="btn btn-info btn-md" style="font-weight:bold;background-color:#8b9dc3;border-color:#8b9dc3;" onclick="window.location='https://www.facebook.com/pages/WakeWank/311905402310266';">
+					<img src="css/static/facebook.png" height="26" style="margin-top:-20px;margin-bottom:-20px;margin-right:6px;"> Like on Facebook&nbsp;&nbsp;
+				</button>
+				
+			</div>
+			</a>
+            
+            <a>
+			<div class="far-right2 hidden-xs">
+				
+				<button type="button" class="btn btn-info btn-md" style="font-weight:bold;">
+					<img src="css/static/twitter.png" height="25" style="margin-top:-20px;margin-bottom:-20px;margin-right:2px;margin-left:-2px;" onclick="window.location='https://twitter.com/WakeWankster'"> Follow on Twitter&nbsp;
+				</button>
+				
+			</div>
+			</a>
+			
+			<a data-toggle="modal" data-target="#myModal32">
+			<div class="far-right hidden-xs">
+					<button type="button" class="btn btn-default btn-md" style="font-weight:bold;" onclick="window.location='https://www.youtube.com/channel/UCxQgD16--WEAfHBryFCpM_w'">
+						<img src="css/static/youtube.png" height="25" style="margin-top:-23px;margin-bottom:-23px;margin-right:3.5px;margin-left:-3px;"> YouTube Channel</button>
+			</div>
+			</a>
+			<div class="far-right3 hidden-xs">
+				<h3 class="white-text small" style="font-size:16pt;line-height:24pt">Wake up to videos of beautiful women. Start every morning with a happy beginning!</h3>
+			</div>
+		</div>
+		
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		  <div class="modal-dialog" style="margin-top:15%;">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+		        <h4 class="modal-title" id="myModalLabel">Sign Up</h4>
+		      </div>
+		      <div class="modal-body">
+		        <h5 class="">Please enter your preferred username:</h5>
+		        <div class="form-group">
+				    <div class="input-group">
+				      <div class="input-group-addon">@</div>
+				      <input class="form-control" type="email" placeholder="Username">
+				    </div>
+				  </div>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary btn-color">Subscribe</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+		<script type="text/javascript">
+			$(document).ready(function() {
+					$(".loader-image").fadeOut(1500, function() {
+							$("#loader").fadeOut(1500);
+					});
+					
+			});
+			setInterval(function() {
+							$('.arrow').effect('bounce',{times:1, distance:15}, 1200)
+			}, 200);
+			$(document).ready(function() {
+                $('.main-text').css('font-size', ($(window).height()*0.02)+12+'px');
+                $('.logo').css('margin-bottom', ($(window).height()*0.025)-20+'px');
+                $('.android-btn-real-min').css('width', ($(window).height()*0.1)+100+'px');
+                $('.android-btn-real-img').css('margin-top', ($(window).height()*0.008)-10+'px');
+                $('.logo').css('max-width', ($(window).height()*$(window).height()*.0009)+15+'px');
+                $('.logo2').css('margin-top', ($(window).width()*-0.08)+45+'%');
+                $('.letters').css('font-size', ($(window).width()*-0.1)+24+'px');
+
+			    /* Every time the window is scrolled ... */
+			    $(window).scroll( function(){
+			    
+			        /* Check the location of each desired element */
+			        $('.fun').each( function(i){
+			            
+			            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			            var bottom_of_window = $(window).scrollTop() + $(window).height();
+			            
+			            /* If the object is completely visible in the window, fade it it */
+			            if( bottom_of_window > bottom_of_object ){
+			                
+			                $(this).fadeIn(2500);
+			                    
+			            }
+			            
+			        }); 
+			    
+					});
+    
+			});
+		</script>
+        <script type="text/javascript">
+            $element = $('.main-top');
+            function fixMobileSafariViewport() {
+              $element.css('height', window.innerHeight * 1);
+            }
+            // listen to portrait/landscape changes
+            window.addEventListener('orientationchange', fixMobileSafariViewport, true);
+            fixMobileSafariViewport();
+        </script>	</body>
+</html>
